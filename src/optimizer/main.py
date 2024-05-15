a2c9be104a9c4a39cc385f4ab1bd38ac4a97cfaa
room-208 <yumalegomindstorm@gmail.com>
2024-05-15 11:35:46 +0900

    add：cleanup_csvを追加しました

--- src/optimizer/main.py
+++ src/optimizer/main.py
@@ -1,12 +1,15 @@
 import random
 
-from common.const import SEED
+from common.cleanup import cleanup_csv
+from common.const import OUTPUTS_DIR, SEED
 from common.seed import seed_everything
 from optimizer.data_structure.state import State
 from optimizer.neighborhood.insert import insert
 
 if __name__ == "__main__":
     seed_everything(SEED)
+
+    cleanup_csv(OUTPUTS_DIR)
 
     state = State()
     state.writeAssignments()
